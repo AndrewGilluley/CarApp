db38ca9e354b93ba2fce4021bcf5e94be4872caa
Andrew.Gilluley <Andrewgilluley96@gmail.com>
2020-07-26 20:10:53 +0100

    Updates

---
+++ precache-manifest.0acc0973a73e541a10305aa08ec75228.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5fb17a3cb50f125018a361c0e8c884",
+    "url": "/CarApp/index.html"
+  },
+  {
+    "revision": "8cac0d234481a04379cf",
+    "url": "/CarApp/static/css/main.a5abcc43.chunk.css"
+  },
+  {
+    "revision": "65ed270902965eee655e",
+    "url": "/CarApp/static/js/2.15b3d2e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CarApp/static/js/2.15b3d2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cac0d234481a04379cf",
+    "url": "/CarApp/static/js/main.0daa19c0.chunk.js"
+  },
+  {
+    "revision": "2aac8dcd1d7c0cf73b29",
+    "url": "/CarApp/static/js/runtime-main.a3db4de8.js"
+  },
+  {
+    "revision": "6615e31db8d5e0b02aed720bcfdaa1c4",
+    "url": "/CarApp/static/media/car-hero.6615e31d.jpeg"
+  },
+  {
+    "revision": "ad46b21e4dc9b94e231414d929222ef1",
+    "url": "/CarApp/static/media/dodge-1.ad46b21e.jpeg"
+  },
+  {
+    "revision": "d713f6282df9e9d886bba9173efca926",
+    "url": "/CarApp/static/media/dodge-2.d713f628.jpeg"
+  },
+  {
+    "revision": "146b84a1e4698213446cd4c26fe3aaeb",
+    "url": "/CarApp/static/media/dodge-3.146b84a1.jpeg"
+  },
+  {
+    "revision": "0e071094445cdf977eef73509d2c330c",
+    "url": "/CarApp/static/media/dodge-4.0e071094.jpeg"
+  },
+  {
+    "revision": "a2cf7a0e197d84aad0b8f6fbee9b9249",
+    "url": "/CarApp/static/media/ford-1.a2cf7a0e.jpeg"
+  },
+  {
+    "revision": "d5e4ab9368f5bf511ead4366857a9cd4",
+    "url": "/CarApp/static/media/ford-2.d5e4ab93.jpeg"
+  },
+  {
+    "revision": "02a7dad01fd91f43bf1b0e0d35c69542",
+    "url": "/CarApp/static/media/ford-3.02a7dad0.jpeg"
+  },
+  {
+    "revision": "d158e5615e251dd464f6c4253391383c",
+    "url": "/CarApp/static/media/ford-4.d158e561.jpeg"
+  },
+  {
+    "revision": "d0a6e242ddb44024b9caf6ba162df18d",
+    "url": "/CarApp/static/media/hero.d0a6e242.jpeg"
+  },
+  {
+    "revision": "f97e7f5729e25dd2172593c713a48eac",
+    "url": "/CarApp/static/media/inside-1.f97e7f57.jpeg"
+  },
+  {
+    "revision": "722fbea10deb769627417eeba627acb2",
+    "url": "/CarApp/static/media/inside-2.722fbea1.jpeg"
+  },
+  {
+    "revision": "588b97470be6862e50de8a3daa6e6a7c",
+    "url": "/CarApp/static/media/inside-3.588b9747.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/CarApp/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "da5db43c0d3568a48bb8b1b999cc7754",
+    "url": "/CarApp/static/media/nissan-1.da5db43c.jpeg"
+  },
+  {
+    "revision": "722e8bc37f236a7c38c982c0f9c4e099",
+    "url": "/CarApp/static/media/nissan-2.722e8bc3.jpeg"
+  },
+  {
+    "revision": "086ebf8ded8e58cebeefbae5d43d4965",
+    "url": "/CarApp/static/media/nissan-3.086ebf8d.jpeg"
+  },
+  {
+    "revision": "3b2539c72b8c46c378564af4551b62af",
+    "url": "/CarApp/static/media/nissan-4.3b2539c7.jpeg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/CarApp/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "25837daa6b18ac1e6ef215c85bc007f7",
+    "url": "/CarApp/static/media/tesla-1.25837daa.jpg"
+  }
+]);
